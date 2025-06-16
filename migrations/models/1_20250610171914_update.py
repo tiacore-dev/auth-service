5d78c91322c0c8e923873008f62da3d7dc2071bb
Viktoria <v.s.polikanova@gmail.com>
2025-06-16 15:01:12 +0700

    небольшие изменения в миграции

--- migrations/models/1_20250610171914_update.py
+++ migrations/models/1_20250610171914_update.py
@@ -3,9 +3,10 @@ from tortoise import BaseDBAsyncClient
 
 async def upgrade(db: BaseDBAsyncClient) -> str:
     return """
+        DROP TABLE IF EXISTS "entity_company_relations";
         DROP TABLE IF EXISTS "legal_entities";
         DROP TABLE IF EXISTS "legal_entity_types";
-        DROP TABLE IF EXISTS "entity_company_relations";"""
+    """
 
 
 async def downgrade(db: BaseDBAsyncClient) -> str:
